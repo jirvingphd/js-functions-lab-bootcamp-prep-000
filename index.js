fed63edd2627c28f9a00851e1ef472878c4dcb22
James M. Irving <james.irving.phd@outlook.com>
2018-11-08 23:12:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // write your code below!
+function happyHolidays(){
+return("Happy holidays!")
+}
+
+function happyHolidaysTo(name){
+return("Happy holidays, ${name}!")
+
+}
+
+function happyHolidayTo(string1, string2){
+  
+  
+}
+
+function holidayCountdown(holName, days){
+  
+  
+}
